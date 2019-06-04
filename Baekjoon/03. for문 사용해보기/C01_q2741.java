91eb0ea8ab1494a7b45c4ab208e63c57a8251a30
cg2z <97cg2z@gmail.com>
2019-06-04 12:36:18 +0900

    .

---
+++ Baekjoon/03. for문 사용해보기/C01_q2741.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class C01_q2741 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		for(int i=1;i<=num;i++)
+			System.out.println(i);
+	}
+}
