317fc3ed0d5334f88825e4550210f0c0d9b8bc8a
cg2z <97cg2z@gmail.com>
2019-06-05 18:49:44 +0900

    .

---
+++ Baekjoon/03. for문 사용해보기/C02_q2742.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class C02_q2742 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		for (int i = num; i > 0; i--)
+			System.out.println(i);
+	}
+}
