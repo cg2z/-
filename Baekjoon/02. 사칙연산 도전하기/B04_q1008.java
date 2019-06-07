28ab351fdee698182dde88f36464c63f80644a51
cg2z <97cg2z@gmail.com>
2019-06-07 18:15:07 +0900

    .

---
+++ Baekjoon/02. 사칙연산 도전하기/B04_q1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class B04_q1008 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		double num = scanner.nextInt();
+		double num2 = scanner.nextInt();
+		System.out.print(num/num2);
+	}
+}
