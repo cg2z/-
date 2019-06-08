635e5b874de3357c7e9f0098db9978dc46894635
cg2z <97cg2z@gmail.com>
2019-06-09 00:58:58 +0900

    .

---
+++ 백준/02. 사칙연산 도전하기/B07_q2558.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class B07_q2558 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		int num2 = scanner.nextInt();
+		System.out.print(num+num2);
+	}
+}
