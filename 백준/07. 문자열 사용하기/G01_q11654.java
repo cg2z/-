41d967829a296c73a5e36a771ee6e9bbd4801eb8
cg2z <97cg2z@gmail.com>
2019-06-22 17:16:52 +0900

    .

---
+++ 백준/07. 문자열 사용하기/G01_q11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class G01_q11654 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.next();
+		char ch = str.charAt(0);
+		System.out.println((int)ch);
+	}
+}
