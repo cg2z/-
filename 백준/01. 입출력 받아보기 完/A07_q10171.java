3405dfb10951a43ec7b15d51827edef74e0b12a4
cg2z <97cg2z@gmail.com>
2019-06-26 21:21:35 +0900

    .

---
+++ 백준/01. 입출력 받아보기 完/A07_q10171.java
@@ -0,0 +1,8 @@
+public class A07_q10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.print(" \\(__)|");
+	}
+}
