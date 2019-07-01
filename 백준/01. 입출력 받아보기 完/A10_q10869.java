e272838beba47b360e565b46636149501d5696e3
cg2z <97cg2z@gmail.com>
2019-07-02 05:42:03 +0900

    .

---
+++ 백준/01. 입출력 받아보기 完/A10_q10869.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B05_q10869 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		int num2 = scanner.nextInt();
+		System.out.println(num+num2);
+		System.out.println(num-num2);
+		System.out.println(num*num2);
+		System.out.println(num/num2);
+		System.out.println(num%num2);
+	}
+}
